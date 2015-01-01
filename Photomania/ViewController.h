31064323b75b4bd1f27e5c0d55bf3180577c7b1a
Andrew Codispoti <andrewcodispoti@Andrews-Mac-mini.local>
2015-01-01 14:17:51 -0500

    Initial Commit

---
+++ Photomania/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photomania
+//
+//  Created by Andrew Codispoti on 2015-01-01.
+//  Copyright (c) 2015 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Photomania/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Photomania
+//
+//  Created by Andrew Codispoti on 2015-01-01.
+//  Copyright (c) 2015 Andrew Codispoti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
